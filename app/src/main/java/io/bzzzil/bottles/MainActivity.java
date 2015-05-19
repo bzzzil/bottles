8d2b0ba0ef2dcc4dd4477d76ebd4a9fcb1cfc80a
Vasily Melenchuk <bzzzil@gmail.com>
2015-05-19 20:48:19 +0300

    dummy population of list view

--- app/src/main/java/io/bzzzil/bottles/MainActivity.java
+++ app/src/main/java/io/bzzzil/bottles/MainActivity.java
@@ -1,9 +1,16 @@
 package io.bzzzil.bottles;
 
+import android.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +19,18 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView bottlesList = (ListView)findViewById(R.id.listViewBottles);
+        String[] items = { "Alef", "Bet", "Gimel", "Alef", "Bet", "Gimel","Alef", "Bet", "Gimel","Alef", "Bet", "Gimel","Alef", "Bet", "Gimel","Alef", "Bet", "Gimel","Alef", "Bet", "Gimel","Alef", "Bet", "Gimel","Alef", "Bet", "Gimel","Alef", "Bet", "Gimel"};
+        ArrayAdapter<String> bottlesListStaticAdapter = new ArrayAdapter<>( this, R.layout.main_bottleslist_item, items );
+        bottlesList.setAdapter( bottlesListStaticAdapter );
+
+        bottlesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d( "OOO", "Clicked" + id );
+            }
+        });
     }
 
 
