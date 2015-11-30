ac4c1536bb0a17341d3465900df88c79211f5ed0
Vasily Melenchuk <bzzzil@gmail.com>
2015-11-30 22:15:40 +0300

    chg: indicate local sources changes in about window fix: code cleanup

--- app/src/main/java/io/bzzzil/bottles/AboutActivity.java
+++ app/src/main/java/io/bzzzil/bottles/AboutActivity.java
@@ -1,29 +1,24 @@
 package io.bzzzil.bottles;
 
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
-import io.bzzzil.bottles.database.BottlesContentProvider;
-
 public class AboutActivity extends AppCompatActivity {
-
-    private static final String TAG = "AboutActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        ((TextView)findViewById(R.id.about_text)).setText(
-                "RSA: " + getString(R.string.git_rsa).substring(0,6)
-        );
+        StringBuilder about = new StringBuilder();
+
+        about.append("RSA: " + getString(R.string.git_rsa).substring(0, 6));
+
+        if (!getString(R.string.git_local_changes).isEmpty()) {
+            about.append(" + LOCAL CHANGES");
+        }
+
+        ((TextView)findViewById(R.id.about_text)).setText(about);
     }
 }
